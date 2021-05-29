3f151e352c5d8099fb9112f4a446542f879653c8
Андрей <twinka4ik174@mail.ru>
2021-05-29 20:57:41 +0500

    fix templates

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -17,7 +17,8 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = 'django-insecure-vfxph6_*py0m$64lq9b4%sc&cs46pmva$splmub%)osofp5e^y'
+DEBUG = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
