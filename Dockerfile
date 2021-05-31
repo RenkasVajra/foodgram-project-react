5b1aff8fc8a2a27a9ec40a4fe2d06dd814a54d63
Андрей <twinka4ik174@mail.ru>
2021-05-31 21:22:39 +0500

    end)))

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.8.5
+
+RUN pip install --upgrade pip
+
+RUN mkdir /code
+
+COPY requirements.txt /code
+
+RUN pip3 install -r /code/requirements.txt
+
+COPY . /code
+
+WORKDIR /code
+
+RUN python --version; pip --version
+
+CMD gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
--- foodgram/settings.py
+++ foodgram/settings.py
@@ -97,9 +97,9 @@ WSGI_APPLICATION = "foodgram.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(os.path.join(BASE_DIR, "db.sqlite3")),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
