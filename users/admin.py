8ea8608eef610161a6f3146ee425f505f01073b9
Андрей <twinka4ik174@mail.ru>
2021-07-12 20:15:23 +0500

    fix admin

--- users/admin.py
+++ users/admin.py
@@ -4,6 +4,7 @@ from .models import User
 
 
 class UserAdmin(admin.ModelAdmin):
+    model = User
     list_display = ("first_name", "last_name", "username", "email")
     list_filter = ("email", "username", "first_name")
     search_fields = ("email", "first_name")
